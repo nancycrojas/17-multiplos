e06324721719c450927d2b80270f66921bb1f119
nancycrojas <nanclarisaok@gmail.com>
2022-09-30 02:09:55 -0300

    Cambiando nombre de variable

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 //Crear un programa que pida al usuario un número, luego otro y mostrar en un mensaje si el primer valor es múltiplo del segundo. INVESTIGAR EL OPERADOR MÓDULO O DE RESTO % PARA HACERLO
 
 const numero = prompt("Por favor ingresa un número");
-const numero2 = prompt("Por favor ingresa otro número");
+const numeroDos = prompt("Por favor ingresa otro número");
 
-alert(`¿El valor ${numero} es múltiplo de ${numero2}?: ${numero % numero2 === 0}`)
+alert(`¿El valor ${numero} es múltiplo de ${numeroDos}?: ${numero % numeroDos === 0}`);
 
-//alert("¿El valor " + numero + " es multiplo de " + numero2 + "?: " + (numero % numero2 === 0))
+//alert("¿El valor " + numero + " es múltiplo de " + numeroDos + "?: " + (numero % numeroDos === 0))
